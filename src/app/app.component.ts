93e8aa6c24d62be83b0cdc4224828cb9b0b74e0b
luizotcunha <luizotcunha@hotmail.com>
2018-11-24 11:28:21 -0300

    new file:   .editorconfig 	new file:   .gitignore 	new file:   angular.json 	new file:   e2e/protractor.conf.js 	new file:   e2e/src/app.e2e-spec.ts 	new file:   e2e/src/app.po.ts 	new file:   e2e/tsconfig.e2e.json 	new file:   package-lock.json 	new file:   package.json 	new file:   src/app/app-routing.module.ts 	new file:   src/app/app.component.css 	new file:   src/app/app.component.html 	new file:   src/app/app.component.spec.ts 	new file:   src/app/app.component.ts 	new file:   src/app/app.module.ts 	new file:   src/assets/.gitkeep 	new file:   src/browserslist 	new file:   src/environments/environment.prod.ts 	new file:   src/environments/environment.ts 	new file:   src/favicon.ico 	new file:   src/index.html 	new file:   src/karma.conf.js 	new file:   src/main.ts 	new file:   src/polyfills.ts 	new file:   src/styles.css 	new file:   src/test.ts 	new file:   src/tsconfig.app.json 	new file:   src/tsconfig.spec.json 	new file:   src/tslint.json 	new file:   tsconfig.json 	new file:   tslint.json
    	src/app/formulario-lanc/

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'eai';
+  nome = 'Joao';
+  nome2 = 'Maria';
+
+  exibirAlerta() {
+    alert(this.nome2);
+    console.log(this.nome2);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormularioLancComponent } from './formulario-lanc/formulario-lanc.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormularioLancComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
